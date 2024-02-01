97324c12fc0429ac4a7bfa6ea34815b8e7d3830d
snow <andy0509rey@gmail.com>
2024-02-01 23:11:47 +0100

    Smile

--- src/SmileyFaces.java
+++ src/SmileyFaces.java
@@ -5,8 +5,6 @@ import java.util.*;
 public class SmileyFaces {
     public static int countSmileys(List<String> arr) {
 
-
-
         String[] eyes = {":", ";"};
         String[] nose = {"-", "~", ""};
         String[] mouth = {")", "D"};
@@ -14,10 +12,8 @@ public class SmileyFaces {
         int result = 0;
 
         for (int i = 0; i < arr.size(); i++) {
-            String now = arr.get(i);
-            System.out.println(now);
-            if (arr.get(i).contains(eyes[0]) || arr.get(i).contains(eyes[1])) {
-                if (arr.get(i).contains(nose[0]) || arr.get(i).contains(nose[1]) || arr.get(i).contains(nose[2])) {
+            if (arr.get(i).length() >= 2 && (arr.get(i).contains(eyes[0]) || arr.get(i).contains(eyes[1]))) {
+                if (arr.get(i).length() == 2 || (arr.get(i).length() == 3 && isValidNose(arr.get(i).charAt(1), nose))) {
                     if (arr.get(i).charAt(arr.get(i).length() - 1) == ')' || arr.get(i).charAt(arr.get(i).length() - 1) == 'D') {
                         result++;
                     }
@@ -28,5 +24,35 @@ public class SmileyFaces {
         System.out.println("My result is " + result);
 
         return result;
+
     }
+    private static boolean isValidNose(char noseChar, String[] nose) {
+        String noseString = String.valueOf(noseChar);
+        return noseString.equals(nose[0]) || noseString.equals(nose[1]) || noseString.equals(nose[2]);
+    }
+
+    /*
+
+    import java.util.*;
+
+    public class SmileFaces {
+
+     static String validSmilyFaces =  ":) ;) :-) :~) ;-) ;~) :D ;D :-D :~D ;~D ;~) ;-D";
+
+
+      public static int countSmileys(List<String> arr) {
+          int counter = 0;
+
+          for (int i =0; i < arr.size(); i++){
+
+            if (validSmilyFaces.contains(arr.get(i))){
+                counter++;
+            }
+          }
+          return counter;
+      }
+    }
+
+     */
+
 }
